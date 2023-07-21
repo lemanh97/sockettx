1cc161d7d9f7865bd1e714c12b3ba9b2bea2883f
lemanh97 <dieuhien4996@gmail.com>
2023-07-21 21:45:17 +0700

    fix url

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ var io = socketIO(server,
 });
 var cuoc   = Array();
 var system = {
-	url : 'https://demo1.cardvip365.com', 
+	url : 'https://api.benat.net', 
 	keycode : '123456', 
 	ngocrong_min : 1000, 
 	ngocrong_max : 2000,
